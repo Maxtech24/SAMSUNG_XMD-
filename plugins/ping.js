38d195303f632c915a490532d84f730b558f99c0
Maxtech24 <shafanipro24@gmail.com>
2025-08-06 07:57:20 +0300

    max

--- plugins/ping.js
+++ plugins/ping.js
@@ -28,7 +28,7 @@ async function pingCommand(sock, chatId, message) {
         const uptimeInSeconds = process.uptime();
         const uptimeFormatted = formatTime(uptimeInSeconds);
 
-        const botInfo = `🔸 *Speed:* ${ping} ms`.trim();
+        const botInfo = `🫆 *Speed:* ${ping} ms`.trim();
 
         // Reply to the original message with the bot info
         await sock.sendMessage(chatId, { text: botInfo},{ quoted: message });
